23cdeb4b3e8e96cfc10f56ef333c15d72d8d106d
MatiCh01 <exechoco17@gmail.com>
2026-09-10 14:41:54 -0300

    feat: Estructura base de carpetas y enrutamiento App Router.

---
+++ front/src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-2xl font-bold">Panel Principal (Dashboard)</h1>
+    </main>
+  );
+}
