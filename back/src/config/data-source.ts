4b1ede40128e58f9c3e813667a6b1bca72119a0a
NicolasGScenna <nicolasgabrielscenna@gmail.com>
2026-09-10 14:40:27 -0300

    wip:db created, auth-module pending

---
+++ back/src/config/data-source.ts
@@ -0,0 +1,9 @@
+import { DataSource } from "typeorm";
+import 'dotenv/config';
+
+export default new DataSource({
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    entities:['src/**/*.entity.ts'],
+    migrations:['src/migrations/*ts'],
+});
