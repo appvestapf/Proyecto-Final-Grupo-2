1a4ec2fbd71fdda0334a1d0f8f61bbc090d95493
FacundoGalvan <facundogalvan097@gmail.com>
2026-09-14 17:03:36 -0300

    fix: eliminar restricciones de ancho y margenes en el layout y importar hero en el home

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Hero } from '@/components/home/Hero/Hero'; 
+
 export default function Home() {
   return (
-    <main className="min-h-screen bg-base-1 p-8">
+    <main className="flex flex-col min-h-screen bg-base-1">
+      <Hero />      
     </main>
   );
 }
