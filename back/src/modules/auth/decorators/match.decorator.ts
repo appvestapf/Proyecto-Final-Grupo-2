d69faee24e13cae10e8fbd1c8625a39909ca8cde
NicolasGScenna <nicolasgabrielscenna@gmail.com>
2026-09-17 14:06:41 -0300

    fixed auth

--- back/src/modules/auth/decorators/match.decorator.ts
+++ back/src/modules/auth/decorators/match.decorator.ts
@@ -1,24 +1,29 @@
-import { registerDecorator, ValidationOptions, ValidationArguments } from "class-validator";
+import {registerDecorator,ValidationArguments,ValidationOptions} from 'class-validator';
 
 export function Match(
-    property: string,
-    validationOptions?: ValidationOptions,
-){
-    return function(object:object, propertyName: string){
-        registerDecorator({
-            name: 'Match',
-            target: object.constructor,
-            propertyName: propertyName,
-            options: validationOptions,
-            validator: {
-                validate(value:any,args: ValidationArguments) {
-                    const [relatedPropertyName] = args.constraints;
-                    const relatedValue = (args.object as any)[
-                        relatedPropertyName
-                    ]
-                    return value === relatedValue
-                }
-            }
-        })
-    }
+  property: string,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      name: 'Match',
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+
+      validator: {
+        validate(value: unknown, args: ValidationArguments) {
+          const relatedValue = (args.object as Record<string, unknown>)[
+            property
+          ];
+
+          return value === relatedValue;
+        },
+
+        defaultMessage() {
+          return `${propertyName} debe coincidir con ${property}`;
+        },
+      },
+    });
+  };
 }
