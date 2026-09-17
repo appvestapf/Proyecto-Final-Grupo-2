4496b3536ba0cfed51568de745bc0d56ece9685e
SarhenRamirez <sarhenramirez2@gmail.com>
2026-09-17 14:41:28 -0500

    feat: enviar mail de bienvenida al registrarse (email/password y Google)

--- back/src/modules/auth/auth.module.ts
+++ back/src/modules/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { JwtModule } from "@nestjs/jwt";
 import { UsersModule } from "../users/users.module";
+import { MailModule } from "../mail/mail.module";
 import { AuthService } from "./auth.service";
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
@@ -8,6 +9,7 @@ import { GoogleStrategy } from "./strategies/google.strategy";
 @Module({
     imports: [
         UsersModule,
+        MailModule,
         JwtModule.register({
             secret: process.env.JWT_SECRET,
             signOptions: {
