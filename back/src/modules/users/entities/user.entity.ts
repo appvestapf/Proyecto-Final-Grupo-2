be812a650f6eb724f4427279d9d2be4b490b6dbd
SarhenRamirez <sarhenramirez2@gmail.com>
2026-09-18 14:34:27 -0500

    feat: implementar modulo Appointments (citas) con JWT y relaciones a User/Property

--- back/src/modules/users/entities/user.entity.ts
+++ back/src/modules/users/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import { Reservation } from '../../reservations/entities/reservation.entity';
+import { Appointment } from '../../appointments/entities/appointment.entity';
 import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { ApiHideProperty, ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Exclude } from 'class-transformer';
@@ -41,11 +42,8 @@ export class User {
   @OneToMany(()=> Reservation, (reservation)=> reservation.user)
   reservations: Reservation[];
 
-  //@OneToMany(() => Cita, (cita) => cita.user)
-  //citas: Cita[];
-
-  //@OneToMany(() => Reserva, (reserva) => reserva.user)
-  //reservas: Reserva[];
+  @OneToMany(() => Appointment, (appointment) => appointment.user)
+  appointments: Appointment[];
 
   @BeforeInsert()
   @BeforeUpdate()
