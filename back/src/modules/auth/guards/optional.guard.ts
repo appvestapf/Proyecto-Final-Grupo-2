7b2ad3e8e037e2ccad3e02599fa5f6bb6fac7360
Luciano85 <maranonluciano85@gmail.com>
2026-09-23 18:03:36 -0300

    chore: sacar console.log de debug y import sin usar en OptionalJwtAuthGuard

--- back/src/modules/auth/guards/optional.guard.ts
+++ back/src/modules/auth/guards/optional.guard.ts
@@ -1,13 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { info } from 'console';
 
 @Injectable()
 export class OptionalJwtAuthGuard extends AuthGuard('jwt') {
   handleRequest(err: any, user: any) {
-    console.log('GUARD - err:', err);
-    console.log('GUARD - user:', user);
-    console.log('GUARD - info:', info);
     return user;
   }
 }
